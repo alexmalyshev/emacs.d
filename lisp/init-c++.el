71b0d26606ea774be075914e375fe4706b9aabdc
Alexander Malyshev <lex.malyshev@gmail.com>
2018-09-05 02:16:31 -0700

    Fixes for C++ package
    * c-basic-offset should be using defconst instead of defvar.
    
    * flycheck needs to be told to use a newer language standard.
    
    * Try using a local function for the init hook.

--- lisp/init-c++.el
+++ lisp/init-c++.el
@@ -2,7 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
-(defvar c-basic-offset 2)
+(defconst c-basic-offset 2)
 
 (defun set-c++-offsets ()
   "Set C++ indentation offsets to my rules.
@@ -13,18 +13,18 @@ Don't indent on opening a C++ namespace, an extern block, or on case labels."
   (c-set-offset 'arglist-intro '+)
   (c-set-offset 'arglist-close 0))
 
+(defconst flycheck-clang-language-standard "c++17")
+
 (defun set-clang-format-shortcuts ()
   "Set keyboard shortcuts for running clang-format."
   (local-set-key (kbd "C-c r") 'clang-format-region)
   (local-set-key (kbd "C-c b") 'clang-format-buffer))
 
-(defun init-c++-hook ()
-  "Stuff to run when entering `c++-mode'."
-  (set-c++-offsets)
-  (set-clang-format-shortcuts)
-  (flycheck-mode -1))
-
-(add-hook 'c++-mode-hook 'init-c++-hook)
+(let ((hook (lambda ()
+              "Stuff to run when entering `c++-mode'."
+              (set-clang-format-shortcuts)
+              (flycheck-mode -1))))
+  (add-hook 'c++-mode-hook hook))
 
 (provide 'init-c++)
 ;;; init-c++.el ends here
