6928c60b433d94c28c6c85dcd0ee08a4e2d16e4b
Alexander Malyshev <lex.malyshev@gmail.com>
2018-09-05 02:31:05 -0700

    Use init-c++ prefix for all local C++ package functions
    Giving up on trying to use local lambdas and the like.

--- lisp/init-c++.el
+++ lisp/init-c++.el
@@ -3,8 +3,9 @@
 ;;; Code:
 
 (defconst c-basic-offset 2)
+(defconst flycheck-clang-language-standard "c++17")
 
-(defun set-c++-offsets ()
+(defun init-c++--set-c++-offsets ()
   "Set C++ indentation offsets to my rules.
 Don't indent on opening a C++ namespace, an extern block, or on case labels."
   (c-set-offset 'inextern-lang 0)
@@ -13,18 +14,18 @@ Don't indent on opening a C++ namespace, an extern block, or on case labels."
   (c-set-offset 'arglist-intro '+)
   (c-set-offset 'arglist-close 0))
 
-(defconst flycheck-clang-language-standard "c++17")
-
-(defun set-clang-format-shortcuts ()
+(defun init-c++--set-clang-format-shortcuts ()
   "Set keyboard shortcuts for running clang-format."
   (local-set-key (kbd "C-c r") 'clang-format-region)
   (local-set-key (kbd "C-c b") 'clang-format-buffer))
 
-(let ((hook (lambda ()
-              "Stuff to run when entering `c++-mode'."
-              (set-clang-format-shortcuts)
-              (flycheck-mode -1))))
-  (add-hook 'c++-mode-hook hook))
+(defun init-c++--hook ()
+  "Stuff to run when entering `c++-mode'."
+  (init-c++--set-c++-offsets)
+  (init-c++--set-clang-format-shortcuts)
+  (flycheck-mode -1))
+
+(add-hook 'c++-mode-hook 'init-c++--hook)
 
 (provide 'init-c++)
 ;;; init-c++.el ends here
